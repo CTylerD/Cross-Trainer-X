d0af0aa5c94458d3ded2210375ab44a5a2764549
Jooyoung Yu <yujooyoung123@gmail.com>
2023-11-12 20:04:10 -0500

    tabs

--- frontend/app/(navigation)/tabs.js
+++ frontend/app/(navigation)/tabs.js
@@ -2,21 +2,21 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 
 const Tab = createMaterialBottomTabNavigator();
 
-import WelcomeScreen from '../index'
 import NavScreen from '../nav'
 import LoginScreen from '../login'
 import DashboardScreen from '../(dashboard)/dashboard'
+import WelcomeScreen from '..';
+import { NavigationContainer } from '@react-navigation/native';
 
-
-
-export default function bottomTabs() {
+export default function BottomTabs() {
   return (
+    <NavigationContainer>
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={WelcomeScreen} />
+      <Tab.Screen name="Welcome Screen" component={WelcomeScreen} />
       <Tab.Screen name="Login" component={LoginScreen} />
       <Tab.Screen name="Dev" component={NavScreen} />
       <Tab.Screen name="Dashboard" component={DashboardScreen} />
     </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-  
