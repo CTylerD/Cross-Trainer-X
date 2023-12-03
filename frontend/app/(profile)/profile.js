a824ddca65314f35ceb4e5b7c674e09a653d5e1a
Jooyoung Yu <yujooyoung123@gmail.com>
2023-12-03 01:29:21 -0500

    freeweightspng

--- frontend/app/(profile)/profile.js
+++ frontend/app/(profile)/profile.js
@@ -1,44 +1,126 @@
 import React from "react";
-import { StyleSheet, Pressable, Text, View } from 'react-native';
+import { StyleSheet, Pressable, Text, View, ImageBackground, Image } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import axios from 'axios';
         
 
 export default function ProfileScreen() {
-    const axios = require('axios').default;
-    axios.get('http://localhost8080/users/userId').then(function (response){
-        data = response;
-    })
     return (
-    <View style={styles.container}>
-    <Text style={styles.title}>Profile</Text>
+    <ImageBackground source={require('../../assets/images/freeweights.png')} resizeMode="cover" style={{ width: '100%', height: '100%'}}>
+  <View style={[styles.container]}>
+    <Text style={[styles.title]}>Dashboard</Text>
 
     <View style={styles.rowContainer}>
-        <Card>
-        <Card.Title>Workout Track</Card.Title>
+        <Card containerStyle={{width: 250, height: 250}}>
+        <Card.Title>Today's Workout</Card.Title>
         <Card.Divider/>
-            <Text style={styles.text}>{data.firstName} {data.lastName}</Text>
-        </Card>``
+        <Text style={{ alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: 50,
+                lineHeight: 21,
+                letterSpacing: 0.25,
+                color: 'black', marginLeft: 'auto', marginRight: 'auto', marginTop: 50}}>
 
-        <Card>
-        <Card.Title>Days completed card</Card.Title>
+                  Chest
+                </Text>
+        </Card>
+
+        <Card containerStyle={{width: 250, height: 250}}>
+        <Card.Title>Days Completed</Card.Title>
         <Card.Divider/>
-            <Text style={styles.text}>Text</Text>
+          <Text style={{ alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: 50,
+                lineHeight: 21,
+                letterSpacing: 0.25,
+                color: 'black', marginLeft: 'auto', marginRight: 'auto', marginTop: 50}}>18</Text>
         </Card>
     </View>
     
     <View style={styles.rowContainer}>
-        <Card>
-        <Card.Title>Favorite Workout</Card.Title>
+    <Card containerStyle={{width: 250, height: 250,}}>
+        <Card.Title>Stats</Card.Title>
         <Card.Divider/>
-            <Text style={styles.text}>Text</Text>
+          <Text style={[styles.text, {color:'black'}]}>
+          Avg Workout Time: 54 min 
+          {"\n"}
+          Longest Workout: 93 min
+          {"\n"}
+          Grip Strength: 64 psi
+          {"\n"}
+
+          </Text>
         </Card>
 
-        <Card>
-        <Card.Title>Additional Info Card</Card.Title>
+        <Card containerStyle={{width: 250, height: 250}}>
+        <Card.Title>Calories Burned</Card.Title>
+
         <Card.Divider/>
-            <Text style={styles.text}>Text</Text>
+          <Image style={{alignItems: 'center', justifyContent: 'center', width: 100, height: 100, marginLeft: 'auto', marginRight: 'auto'}} source={require('../../assets/images/fire.png')} />
+          <Text style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: 25,
+                lineHeight: 21,
+                letterSpacing: 0.25,
+                color: 'black', marginLeft: 'auto', marginRight: 'auto', marginTop: 10
+
+  }}>953</Text>
         </Card>
     </View>
-    </View>
+
+  
+  </View>
+    </ImageBackground>
 )}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      
+    },
+    rowContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'row',
+    },
+    title: {
+      fontSize: 50,
+      color: 'white',
+      letterSpacing: 0.25,
+      fontFamily:'Poppins',
+    },
+    text: {
+      fontSize: 16,
+      lineHeight: 40,
+      letterSpacing: 0.25,
+      color: 'black',
+      
+    },
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 4,
+      elevation: 3,
+      backgroundColor: 'white',
+      margin: 12,
+      borderWidth: 1,
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      fontWeight: 'bold'
+    },
+    cardbox: {
+      width: 250,
+      height: 250,
+    },
+    picture: {
+      paddingLeft: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 100,
+      height: 100,
+    },
+  });
